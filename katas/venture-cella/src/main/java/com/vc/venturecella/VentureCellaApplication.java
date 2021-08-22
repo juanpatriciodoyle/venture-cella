73145ffc08caae14e1c17a997fd9d826163c5d80
juanpatriciodoyle <juanpatriciodoyle@gmail.com>
2021-08-22 19:43:57 -0300

    Initialized

---
+++ katas/venture-cella/src/main/java/com/vc/venturecella/VentureCellaApplication.java
@@ -0,0 +1,13 @@
+package com.vc.venturecella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentureCellaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VentureCellaApplication.class, args);
+    }
+
+}
